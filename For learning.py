506842b49abe67d144d71b436fb04c9749282d3d
pesiypes <dan.co6aka@gmail.com>
2022-05-17 18:08:38 +0300

    Add a new file

---
+++ For learning.py
@@ -0,0 +1,14 @@
+# data = [
+#    (82, 191),
+#    (68, 174),
+#    (90, 189),
+#    (73, 179),
+#    (76, 184)
+# ]
+#
+# print(sorted(data, key = lambda x: x[0] / x[1] ** 2))
+# print(min(data, key = lambda x: x[0] / x[1] ** 2))
+
+a = ["это", "маленький", "текст", "обидно"]
+
+print(list(map(str.upper, a)))
